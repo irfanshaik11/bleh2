7e34769acd580eec17ca5ce08d758ca4b4b95bdb
irfanshaik11 <14115825+irfanshaik11@users.noreply.github.com>
2026-01-12 17:41:17 -0800

    edit2

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,63 +1,19 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'Interstate Docs',
-  socials: [
-    {
-      icon: 'github',
-      link: 'https://github.com/interstate-labs'
-    },
-    {
-      icon: 'telegram',
-      link: 'https://t.me/+-i4dP7U2BggxMzAx'
-    },
-  ],
+  title: 'Docs',
   sidebar: [
     {
-      text: 'Intro',
-      link: '/intro',
+      text: 'Terms of Service',
+      link: '/terms-of-service',
     },
     {
-      text: 'Benefits and Risks',
-      link: '/benefits-and-risks',
+      text: 'Privacy Policy',
+      link: '/privacy-policy',
     },
     {
-      text: 'For Validators',
-      items: [
-        {
-          text: 'Quickstart',
-          link: '/validator/quickstart',
-        },
-        {
-          text: 'Optionally Run Your Own RPC',
-          link: '/validator/long-form-setup',
-        },
-        {
-          text: 'How To Run Signer',
-          link: '/validator/how-to-run-signer',
-        },
-        {
-          text: 'Validator Registration For Slashing Insurance',
-          link: '/validator/slashing_insurance_registration'
-        }
-      ]
+      text: 'Fees',
+      link: '/fees',
     },
-    {
-      text: 'For Users',
-      items: [
-        {
-          text: 'Quickstart',
-          link: '/user/quickstart',
-        },
-        {
-          text: 'API Specification',
-          link: '/user/api-specification',
-        },
-        {
-          text: 'Insurance',
-          link: '/user/insurance',
-        }
-      ]
-    }
   ],
 })
